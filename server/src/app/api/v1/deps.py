edb126792193e9f3add069940f1b41c94f037c9c
SK Imtiaj Uddin <imtiaj.kol@gmail.com>
2025-11-01 14:30:19 +0530

    feat(document_processing): made FileRepository, FileService with embedding and s3 managers for document processsing and API endpoints for interacting with these

--- server/src/app/api/v1/deps.py
+++ server/src/app/api/v1/deps.py
@@ -1,9 +1,27 @@
-from fastapi import Depends
+import uuid
+
+from fastapi import Depends, HTTPException, Request, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from app.ai.embedding_manager import EmbeddingInstance
+from app.aws.s3_manager import S3Instance
 from app.db.session import db_session_manager
+from app.repository.file import FileRepository
 from app.repository.user import UserRepository
+from app.service.file import FileService
 from app.service.user import UserService
+
+
+def get_current_user(request: Request) -> uuid.UUID:
+    """
+    Dependency function to get the current user from the request headers.
+    """
+    user_id = request.headers.get("x-user-id")
+    if not user_id:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Unauthorized: Missing user ID."
+        )
+    return uuid.UUID(user_id)
 
 
 def get_user_service(session: AsyncSession = Depends(db_session_manager.get_db)) -> UserService:
@@ -18,3 +36,21 @@ def get_user_service(session: AsyncSession = Depends(db_session_manager.get_db))
     """
     user_repo = UserRepository(db_session=session)
     return UserService(user_repository=user_repo)
+
+
+def get_file_service(session: AsyncSession = Depends(db_session_manager.get_db)) -> FileService:
+    """
+    Dependency function to get a FileService instance.
+
+    Args:
+        session (AsyncSession): The async database session
+
+    Returns:
+        FileService: An instance of FileService with all required dependencies
+    """
+    s3_manager = S3Instance.get_s3_manager()
+    embedding_manager = EmbeddingInstance.get_instance()
+    file_repo = FileRepository(db_session=session)
+    return FileService(
+        s3_manager=s3_manager, embedding_manager=embedding_manager, file_repository=file_repo
+    )
